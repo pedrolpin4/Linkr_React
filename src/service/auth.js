bc339d04f63ddefe5938ad8cda568cee2a3aaccd
Yohan Lopes <yohanhxh@gmail.com>
2021-09-14 16:45:41 -0300

    creating list posts server

--- src/service/auth.js
+++ src/service/auth.js
@@ -3,3 +3,11 @@ import axios from 'axios';
 export const API = axios.create({
     baseURL: "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr",
 })
+
+
+export async function getPosts() {
+    const response = await API.get("/posts").catch(() => false);
+
+    if(response) return response.data;
+    else return false;
+}
