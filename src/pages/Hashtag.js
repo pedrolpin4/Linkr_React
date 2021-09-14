be5a6a7c825c827214aec089262c2c99ff29aa91
EDUARDO PINA <eduardopina@MacBook-Air-de-EDUARDO.local>
2021-09-14 16:04:02 -0300

    hashtag's page structure has been added

--- src/pages/Hashtag.js
+++ src/pages/Hashtag.js
@@ -1,7 +1,58 @@
+import { useEffect, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import UserContext from "../context/UserContext";
+import { getHashtagsPosts } from "../service/auth";
+
+
 function Hashtag() {
+    const { hashtag } = useParams()
+    const [hashtagsPosts, setHashtagsPosts] = useState([])
+    const { userData } = useContext(UserContext)
+    
+    const config = {
+        headers: {
+            "Authorization": `Bearer ${userData.token}` 
+        }
+    }
+
+    function renderHashtagPosts (hashtag, config){
+        getHashtagsPosts(hashtag, config)
+            .then(res => {
+                console.log(res.data);
+                setHashtagsPosts([...res.data.posts])
+            })
+            .catch(alert(`Can't find any posts with the hashtag ${hashtag}`))
+    }
+
+    useEffect(() => renderHashtagPosts(hashtag, config), [])
+
     return (
-        <div>Hashtag</div>
+        <>
+            {/*NavBar*/}
+
+            <PageTitle># {hashtag}</PageTitle>
+
+            <PostsContainer>
+                {/* hashtagsPosts.map(post => (
+                   // post
+                )) */}
+            </PostsContainer>
+
+            {/*sidebar*/}
+        </>
     )
 }
+
+const PageTitle = styled.h1`
+    font-weight: bold;
+    font-size: 43px;
+    color: #FFFFFF;
+`
+
+const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
 
 export default Hashtag;
--- src/service/auth.js
+++ src/service/auth.js
@@ -3,3 +3,11 @@ import axios from 'axios';
 export const API = axios.create({
     baseURL: "https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr",
 })
+
+function getHashtagsPosts(hashtag, config) {
+   return API.get(`/hashtags/${hashtag}/posts`, config)
+}
+
+export {
+    getHashtagsPosts
+}
