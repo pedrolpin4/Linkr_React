6fd86ee6a60da8acc9ff9b8a88fd3aafd8a3751e
EDUARDO PINA <eduardopina@air-de-eduardo.home>
2021-09-15 11:57:34 -0300

    TrendingBar component completed

---
+++ src/components/TrendingBar.js
@@ -0,0 +1,75 @@
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { getHashtags } from "../service/auth"
+import UserContext from "../context/UserContext"
+
+const TrendingBar = () => {
+    const { 
+        userData
+    } = useContext(UserContext)
+    const [trendings, setTrendings] = useState([])
+    
+    const config = {
+        headers: {
+            "Authorization": `Bearer ${userData.token}` 
+        }
+    }
+
+    function pickTrendings(config){
+        getHashtags(config)
+            .then(res => setTrendings([...res.data.hashtags]))
+            .catch(res => console.log(res))
+    }
+
+    useEffect(() => pickTrendings(config), [])
+    return(
+        <TrendingsContainer>
+            <TrendingsBarTitle>trending</TrendingsBarTitle>
+            <HorizontalLine />
+                {trendings.map(trending =>(
+                    <HashtagsName>
+                        <Link to = {`/hashtag/${trending.name}`}>
+                            # {trending.name}                     
+                        </Link>    
+                    </HashtagsName>
+                ))}
+        </TrendingsContainer>
+    )
+}
+
+const TrendingsContainer = styled.div`
+    padding: 9px 0px 30px 0px;
+    width: 301px;
+    background: #171717;
+    border-radius: 16px;
+    @media (max-width: 1000px){
+        display: none;
+    }
+`
+
+const HorizontalLine = styled.div`
+    margin: 12px 0px 22px 0px;
+    width: 301px;
+    border: 1px solid #484848;
+`
+
+const TrendingsBarTitle = styled.h1`
+    font-family: 'Oswald', sans-serif;
+    margin-left: 16px ;
+    font-weight: bold;
+    font-size: 27px;
+    color: #FFFFFF;
+`
+
+const HashtagsName = styled.p`
+    font-family: 'Lato', sans-serif;
+    color: #FAFAFA;
+    margin-left: 16px;
+    font-weight: bold;
+    font-size: 19px;
+    color: #FFFFFF;
+    margin-top: 10px;
+`
+
+export default TrendingBar
--- src/pages/Hashtag.js
+++ src/pages/Hashtag.js
@@ -1,65 +1,7 @@
-import { useEffect, useState, useContext } from "react";
-import { useParams } from "react-router-dom";
-import styled from "styled-components";
-import UserContext from "../context/UserContext";
-import { getHashtagsPosts } from "../service/auth";
-
-
 function Hashtag() {
-    const { hashtag } = useParams()
-    const [hashtagsPosts, setHashtagsPosts] = useState([])
-    const { userData } = useContext(UserContext)
-    
-    const config = {
-        headers: {
-            "Authorization": `Bearer ${userData.token}` 
-        }
-    }
-
-    function renderHashtagPosts (hashtag, config){
-        getHashtagsPosts(hashtag, config)
-            .then(res => {
-                console.log(res.data);
-                setHashtagsPosts([...res.data.posts])
-            })
-            .catch(alert(`Can't find any posts with the hashtag ${hashtag}`))
-    }
-
-   // useEffect(() => renderHashtagPosts(hashtag, config), [])
-
-    return (
-        <>
-            {/*NavBar*/}
-            <Ola>
-                <PostsContainer>
-                    <PageTitle># {hashtag}</PageTitle>
-                    {/* hashtagsPosts.map(post => (
-                    // post
-                    )) */}
-                </PostsContainer>
-
-                {/*sidebar*/}
-            </Ola>
-        </>
+    return(
+        <div>Hashtag</div>
     )
 }
-
-
-const Ola = styled.div`
-    width:100vw;
-    height: 100vh;
-    background-color: #333333;
-`
-
-const PageTitle = styled.h1`
-    font-weight: bold;
-    font-size: 43px;
-    color: #FFFFFF;
-`
-
-const PostsContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-`
 
 export default Hashtag;
