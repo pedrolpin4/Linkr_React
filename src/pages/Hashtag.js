f92f2b006b8a7caab4cb2b7cd1ee8a5e313a2ae6
EDUARDO PINA <eduardopina@MacBook-Air-de-EDUARDO.local>
2021-09-15 12:59:51 -0300

    now wecan map the post from a specific trending topic

--- src/pages/Hashtag.js
+++ src/pages/Hashtag.js
@@ -5,7 +5,7 @@ function Hashtag() {
     const testToken = "09622c1e-d975-46a4-8b15-14063223e383"
     const { hashtag } = useParams()
     const [hashtagPosts, setHashtagPosts] = useState([])
-    const { userData } = useContext(UserContext)
+    // const { userData } = useContext(UserContext)
     
     const config = {
         headers: {
@@ -16,28 +16,19 @@ function Hashtag() {
     function renderHashtagPosts (hashtag, config){
         getHashtagsPosts(hashtag, config)
             .then(res => {
-                console.log(res.data);
                 setHashtagPosts([...res.data.posts])
             })
-            .catch(alert(`Can't find any posts with the hashtag ${hashtag}`))
+            .catch(() => alert(`There was an error while finding the posts from the hashtag ${hashtag}`))
     }
 
    useEffect(() => renderHashtagPosts(hashtag, config), [])
 
     return (
         <>
-            {/*NavBar*/}
-            <Ola>
-                <PostsContainer>
-                    <PageTitle># {hashtag}</PageTitle>
-                    {hashtagPosts.map(post => (
-                     //post
-                     <h2>olá</h2>
-                    ))}
-                </PostsContainer>
-
-                {/*sidebar*/}
-            </Ola>
+            <PageTitle># {hashtag}</PageTitle>
+            {hashtagPosts.map(post => (
+                <h2>olá</h2>
+            ))}
         </>
     )
 }
