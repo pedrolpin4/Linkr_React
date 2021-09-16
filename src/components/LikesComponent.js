93f75419c6c6d07092ce541f43fa8fc1841fd47b
EDUARDO PINA <eduardopina@MacBook-Air-de-EDUARDO.local>
2021-09-16 16:21:01 -0300

    tippyjs styles added

--- src/components/LikesComponent.js
+++ src/components/LikesComponent.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import service from "../service/auth"; 
-import Tippy from "@tippy.js/react"
-
+import Tippy from "@tippyjs/react"
+import "tippy.js/dist/tippy.css"
 
 export default function LikesComponent ( {liked, likes, id}) {
     const [ isLiked, setIsLiked ] = useState(liked);
+    console.log(likes);
 
     const testToken = "5f8eb824-09fe-4ef6-a5ed-a26dbcb1bc10"
 
@@ -34,17 +35,20 @@ export default function LikesComponent ( {liked, likes, id}) {
 
     return (
         <>
+                
             {isLiked 
                 ? 
                 <AiFillHeart  onClick = {() => unLikePost(config, id)} color="#ff0000" size={25} /> 
                 : 
                 <AiOutlineHeart onClick = {() => likePost(config, id)} color="#fff" size={25} />
             }
-
-
-            <p className="likes">
-                {`${likes.length} ${likes.length === 1 ? "like" : "likes"}`}
-            </p>
+            
+        
+            <Tippy content = "likes" placement = "bottom">
+                <p className="likes">
+                    {`${likes.length} ${likes.length === 1 ? "like" : "likes"}`}
+                </p>
+            </Tippy>
         </>
     )
 }
