aba9091c5a558ab1cfadbb4fd3975560576ab84c
Andrezza Souza <andrezzasouza@id.uff.br>
2021-09-17 21:13:06 -0300

    Removes data from local storage when logging out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
 import UserContext from "../context/UserContext";
 import { MdKeyboardArrowDown as ArrowDown, MdKeyboardArrowUp as ArrowUp} from "react-icons/md"
@@ -8,12 +8,26 @@ import { MdKeyboardArrowDown as ArrowDown, MdKeyboardArrowUp as ArrowUp} from "r
 export default function NavBar () {
   const {
     userData
-  } = useContext(UserContext)
+  } = useContext(UserContext);
 
+  console.log(userData)
+
+  const history = useHistory();
   const [openDropdown, setOpenDropdown] = useState(false);
 
   function toggleMenu () {
     setOpenDropdown(!openDropdown);
+    console.log("aqui2");
+  }
+
+  function clearStorage () {
+    window.localStorage.removeItem("userLogin");
+  }
+
+  function logOut () {
+    setOpenDropdown(!openDropdown);
+    clearStorage();
+    history.push("/");
   }
 
 
@@ -29,16 +43,14 @@ export default function NavBar () {
 
         <ProfileImg src={userData.user.avatar} onClick={toggleMenu} />
       </div>
-      <DropdownMenu openDropdown={openDropdown}>
+      <DropdownMenu openDropdown={openDropdown} /*ref={menu}*/>
         <Link to="/my-posts">
           <p>My posts</p>
         </Link>
         <Link to="/my-likes">
           <p>My likes</p>
         </Link>
-        <Link to="/">
-          <p>Logout</p>
-        </Link>
+        <p onClick={logOut}>Logout</p>
       </DropdownMenu>
     </NavBarContainer>
   );
@@ -70,11 +82,15 @@ const ProfileImg = styled.img`
   width: 53px;
   height: 53px;
   border-radius: 26.5px;
-`
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
 const DropdownMenu = styled.div`
   position: fixed;
   top: 72px;
   right: 0;
+  z-index: 150;
   height: 109px;
   padding: 10px 29px 6px 28px;
   border-radius: 0px 0px 0px 20px;
@@ -86,9 +102,13 @@ const DropdownMenu = styled.div`
   letter-spacing: 0.05em;
   color: #ffffff;
   text-align: center;
-  display: ${(props) => props.openDropdown ? 'block' : 'none'};
+  display: ${(props) => (props.openDropdown ? "block" : "none")};
 
   p {
     margin-bottom: 11px;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
   }
 `;
