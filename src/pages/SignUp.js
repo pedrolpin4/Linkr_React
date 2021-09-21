44d38ffef891de24abeef5be7c32d3fdcccbfce8
Yohan Lopes <yohanhxh@gmail.com>
2021-09-21 14:54:28 -0300

    adapting sign up to the new service approach

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,6 @@
 import { Link, useHistory } from "react-router-dom";
 import { useState } from "react";
-import API from "../service/api";
+import auth from '../service/auth';
 import { EnterContainer, LogoHolder, LoginForm, EnterButton, EnterInput } from "../SharedStyles/LogInSignUp";
 
 function SignUp() {
@@ -12,30 +12,7 @@ function SignUp() {
   const [image, setImage] = useState("");
   const [enabled, setEnabled] = useState(true);
 
-  function signUpSuccess(response) {
-    if (response.status === 200 || response.status === 201) {
-      alert(
-        "You account has been created! Now you only need to log in to start having fun! :D"
-      );
-      setEnabled(true);
-      setEmail("");
-      setPassword("");
-      setUsername("");
-      setImage("");
-      history.push("/");
-    }
-  }
-
-  function signUpFailure(response) {
-    if (response.response.status === 403) {
-      alert("This e-mail has already been used to create an account. Please, log in or try to use another e-mail address.");
-    } else {
-      alert("Something went wrong. Please, check the fields and try again.");
-    }
-    setEnabled(true);
-  }
-
-  function createAccount (e) {
+  async function createAccount (e) {
     e.preventDefault();
     setEnabled(false);
 
@@ -45,14 +22,15 @@ function SignUp() {
       username: username,
       pictureUrl: image
     }
-    
-    if (email === "" || password === "" || username === "" || image === "") {
-      alert("Please, fill out the fields below.");
-      setEnabled(true);
+
+    const response = await auth.register(body);
+    if(response.sucess) {
+      alert(response.message)
+      setEnabled(true)
+      history.push("/")
     } else {
-      API.post("/sign-up", body)
-        .then(signUpSuccess)
-        .catch(signUpFailure);
+      setEnabled(true)
+      alert(response.message)
     }
   }
 
--- src/service/auth.js
+++ src/service/auth.js
@@ -1,9 +1,5 @@
 import API from './api';
 
-async function register() {
-
-}
-
 async function login(body) {
     for(const key in body) {
         if(!validateCredentials(body[key])) {
@@ -30,7 +26,39 @@ async function login(body) {
         sucess: true,
         data: response.data
     }
-    return response
+    return response;
+}
+
+async function register(body) {
+    for(const key in body) {
+        if(!validateCredentials(body[key])) {
+            return {
+                sucess: false,
+                message: "Please, fill out the fields below."
+            }
+        }
+    }
+
+    const response = await API.post("sign-up", body)
+        .catch(e => {
+            if(e.response.status === 403) {
+                return {
+                    sucess: false,
+                    message: "This e-mail has already been used to create an account. Please, log in or try to use another e-mail address."
+                }
+            }
+            return {
+                sucess: false,
+                message: "Something went wrong. Please, check the fields and try again."
+            }
+        })
+    
+    if(response.data) return {
+        sucess: true,
+        data: response.data,
+        message: "You account has been created! Now you only need to log in to start having fun! :D"
+    }
+    return response;
 }
 
 function validateCredentials(data) {
