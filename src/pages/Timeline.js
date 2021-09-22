c2bd1937105ab7b36d90318249c10b2dc6af6403
Yohan Lopes <yohanhxh@gmail.com>
2021-09-22 17:30:27 -0300

    fixing conditions of warning message render

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -48,23 +48,21 @@ function Timeline() {
             {
             isLoading
                 ? <Loading spinnerSize={30}/>
-                : following.length === 0
+                : following.length === 0 && posts.length === 0
                     ? <FeedbackMessage text="You don't follow anyone yet, search for someone"/>
-                    : posts.length === 0
-                        ? <FeedbackMessage text="We didn't found any posts :(" />
-                        : posts.map(post => <Post key={post.id}
-                                                  username={post.user.username} 
-                                                  text={post.text}
-                                                  link={post.link}
-                                                  profilePic={post.user.avatar}
-                                                  prevTitle={post.linkTitle}
-                                                  prevImage={post.linkImage}
-                                                  prevDescription={post.linkDescription}
-                                                  likes={post.likes}
-                                                  userId={post.user.id}
-                                                  id={post.id}
-                                                  setNewPosts={setNewPosts}
-                                                  newPosts={newPosts} />)
+                    : posts.map(post => <Post key={post.id}
+                                                username={post.user.username} 
+                                                text={post.text}
+                                                link={post.link}
+                                                profilePic={post.user.avatar}
+                                                prevTitle={post.linkTitle}
+                                                prevImage={post.linkImage}
+                                                prevDescription={post.linkDescription}
+                                                likes={post.likes}
+                                                userId={post.user.id}
+                                                id={post.id}
+                                                setNewPosts={setNewPosts}
+                                                newPosts={newPosts} />)
         }</BaseLayout>
     )
 }
